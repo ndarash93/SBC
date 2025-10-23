cdee70a05f93c0622b8247347c713920ed65dc22
Nick Darash <ndarash@apple.com>
2025-10-22 23:26:25 -0700

    DDR routing

--- Scripts/my_first_script.py
+++ Scripts/my_first_script.py
@@ -1,5 +1,18 @@
 import pcbnew as pcb
 
 board = pcb.GetBoard()
+tracks = board.GetTracks()
+track_names = []
+lengths = {}
+for track in tracks:
+  if track.GetNetClassName() == 'DDR_ADDRESS':
+    if track.GetNetname() in track_names:
+      lengths[track.GetNetname()] += track.GetLength()
+    else:
+      track_names.append(track.GetNetname())
+      lengths[track.GetNetname()] = track.GetLength()
+    
 
-print(board)
+print(track_names)
+for key, val in lengths.items():
+  print(key, round(val/1000000, 4))
--- Scripts/pcb_parser.py
+++
@@ -1,2 +0,0 @@
-with open('../SBC.kicad_pcb') as pcb_file:
-  print(pcb_file.read())
