d1269cf171720637ee6f7e5f37ab76f577d98150
Nick Darash <ndarash@apple.com>
2025-10-24 00:46:19 -0700

    DDR Routing more

--- Scripts/my_first_script.py
+++ Scripts/my_first_script.py
@@ -4,15 +4,34 @@ board = pcb.GetBoard()
 tracks = board.GetTracks()
 track_names = []
 lengths = {}
+
+CLK_LEN=50
+RES_LEN=1.6
+
 for track in tracks:
   if track.GetNetClassName() == 'DDR_ADDRESS':
+    length = track.GetLength()
+    name = track.GetNetname().split('/')[-1]
+    name = name.split('DDR_')[-1]
+    if length == 0.0:
+      length = 1545200
     if track.GetNetname() in track_names:
-      lengths[track.GetNetname()] += track.GetLength()
+      lengths[name] += round(length/1000000,4)
     else:
-      track_names.append(track.GetNetname())
-      lengths[track.GetNetname()] = track.GetLength()
+      track_names.append(name)
+      lengths[name] = round(length/1000000,4)
+
+for key, val in lengths.items():
+  print(f"(rule \"AC Signal Length Match {key}\"\n\t(condition \"A.NetName == \'/MEMORY/{key}_R\'\")\n\t(constraint length (min {(CLK_LEN-val-RES_LEN)-1}mm) (max {(CLK_LEN-val-RES_LEN)+1}mm) (opt {(CLK_LEN-val-RES_LEN)}mm)))")
     
 
-print(track_names)
-for key, val in lengths.items():
-  print(key, round(val/1000000, 4))
+"""
+(rule "DDR CLOCK Length Clock"
+	(condition "A.NetClass == 'DDR_CLOCK'")
+	(constraint skew (min -1mm) (max 1mm) (opt 0mm))
+	(constraint length (min 49mm) (max 51mm) (opt 50mm)))
+"""
+#print(track_names)
+#for key, val in lengths.items():
+#  print(key, round(val/1000000, 4))
+
